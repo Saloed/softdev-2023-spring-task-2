5bf6fce5cbdc457a39d851d4ce207ffad23fc68c
Matvey <any@gmail.com>
2023-02-19 14:33:08 +0300

    Message

---
+++ src/test/java/com/tests/AppTest.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class AppTest {
+    try(
+    BufferedReader br = new BufferedReader() {
+
+    } catch(FileNotFoundException e)
+
+    {
+
+    }
+
+    static class InnerTest {
+
+    }
+}
