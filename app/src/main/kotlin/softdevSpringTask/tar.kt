4ea96ce4deebc77183ebc5591b0ad868d19e1edc
Plezha <plehanovtemka@gmail.com>
2023-02-28 15:32:10 +0300

    doesn't even work lol

--- app/src/main/kotlin/softdevSpringTask/tar.kt
+++ app/src/main/kotlin/softdevSpringTask/tar.kt
@@ -22,14 +22,65 @@ fun addFileContent(fileToCopy: File, outputFileOutputStream: OutputStream) {
 }
 
 fun readLine(inputStream: FileInputStream): String {
-    val readChar = { inputStream.read().toChar() }
-    var c = readChar()
+    var c = inputStream.read().toChar()
     var string = ""
     while (c != '\n') {
         string += c
-        c = readChar()
+        c = inputStream.read().toChar()
     }
     return string
+}
+
+fun unarchive(toUnarchive: File) {
+    toUnarchive.inputStream().use { unarchiveInputStream ->
+        val n = readLine(unarchiveInputStream).toInt()
+
+        repeat(n) {
+            val filePathLen = readLine(unarchiveInputStream).toInt()
+            val filePath = readLine(unarchiveInputStream)
+            var remainingFileLength = readLine(unarchiveInputStream).toInt()
+
+            val file = File(filePath)
+            file.outputStream().use { fileOutputStream ->
+                var buffer: ByteArray
+                while (remainingFileLength > 0) {
+                    buffer = ByteArray(min(remainingFileLength, DEFAULT_BUFFER_SIZE))
+                    unarchiveInputStream.read(buffer)
+                    fileOutputStream.write(buffer, 0, min(DEFAULT_BUFFER_SIZE, remainingFileLength))
+
+                    remainingFileLength -= DEFAULT_BUFFER_SIZE
+                }
+            }
+            unarchiveInputStream.skip(1)
+        }
+    }
+}
+
+fun archive(outputFile: File, filePaths: List<String>) {
+    /**
+     * Output file content:
+     * - quantity of files and a line break,
+     * - for every file:
+     *      - filename length in UTF-8 characters and a line break,
+     *      - filename in UTF-8 characters,
+     *      - file content length in bytes and a line break,
+     *      - file content in bytes
+     */
+
+    outputFile.outputStream().use { outputStream ->
+        outputStream.write("${filePaths.size}\n".toByteArray())
+
+        for (i in filePaths.indices) {
+            val file = File(filePaths[i])
+            outputStream.write("${filePaths[i].length}\n".toByteArray())
+            outputStream.write("${filePaths[i]}\n".toByteArray())
+
+            outputStream.write("${file.length()}\n".toByteArray())
+
+            addFileContent(file, outputStream)
+            outputStream.write("\n".toByteArray())
+        }
+    }
 }
 
 @Command(
@@ -54,56 +105,11 @@ class Tar : Runnable {
 
     override fun run() {
         if (unarchiveFile != null) {
-            val toUnarchive = unarchiveFile!!
-
-            toUnarchive.inputStream().use { unarchiveInputStream ->
-                val n = readLine(unarchiveInputStream).toInt()
-
-                repeat(n) {
-                    val filePath = readLine(unarchiveInputStream)
-                    var remainingFileLength = readLine(unarchiveInputStream).toInt()
-
-                    val file = File(filePath)
-                    file.outputStream().use { fileOutputStream ->
-                        while (remainingFileLength > 0) {
-                            val buffer = ByteArray(min(remainingFileLength, DEFAULT_BUFFER_SIZE))
-                            unarchiveInputStream.read(buffer)
-                            fileOutputStream.write(buffer, 0, min(DEFAULT_BUFFER_SIZE, remainingFileLength))
-
-                            remainingFileLength -= DEFAULT_BUFFER_SIZE
-                        }
-                    }
-                    unarchiveInputStream.skip(1)
-                }
-            }
+            unarchive(unarchiveFile!!)
         } else {
-            val outputFile = outputFile
-
-            /**
-             * Output file content:
-             * - quantity of files,
-             * - for every file:
-             *    - filename in UTF-8 characters,
-             *    - file content length in bytes,
-             *    - file content in bytes
-             */
-
-            outputFile.outputStream().use { outputStream ->
-                outputStream.write("${filePaths.size}\n".toByteArray())
-
-                for (i in filePaths.indices) {
-                    val file = File(filePaths[i])
-                    outputStream.write("${filePaths[i]}\n".toByteArray())
-
-                    outputStream.write("${file.length()}\n".toByteArray())
-
-                    addFileContent(file, outputStream)
-                    outputStream.write("\n".toByteArray())
-                }
-            }
+            archive(outputFile, filePaths)
         }
     }
-
 }
 
 
