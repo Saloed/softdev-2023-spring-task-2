abcd4361b83e596c2a80a4e8727dcbcec41d8a5c
Matvey <any@gmail.com>
2023-03-03 13:03:32 +0300

    Changes

--- src/main/java/com/utility/Coder.java
+++ src/main/java/com/utility/Coder.java
@@ -13,53 +13,44 @@ import java.util.List;
  */
 
 public class Coder {
+
     private final static int DEFINE_TYPE = 128;
 
     // method used to encode and write sequence of bytes according to content of "bytes" list
-    public static void printByteArray(List<Integer> bytes, OutputStream bw) throws IOException {
-        if (bytes.isEmpty()) return;
-        else if (bytes.size() == 1 || bytes.get(0) == bytes.get(1)) {
-            bw.write(bytes.size());
-            bw.write(bytes.get(0));
-        } else {
-            bw.write(DEFINE_TYPE + bytes.size());
-            for (Integer b : bytes) bw.write(b);
+    public static void printByteArray(int[] bytes, int size, OutputStream os) throws IOException {
+        if (size == -1) return;
+        os.write(size + 1);
+        for (int i = 0; i <= size; i++) {
+            os.write(bytes[i]);
         }
     }
 
     public static void encode(String inputFile, String outputFile) throws IOException {
         try (InputStream br = new FileInputStream(inputFile);
              OutputStream bw = new FileOutputStream(outputFile)) {
-            List<Integer> bytes = new ArrayList<>();
+            int[] bytes = new int[DEFINE_TYPE - 1];
+            int size = -1;
             while (true) {
-                // Если 128 элементов и последоавтельность неповторяющихся то будет переполнение байта (256)
-                if (bytes.size() == DEFINE_TYPE - 1) {
-                    printByteArray(bytes, bw);
-                    bytes.clear();
-                    continue;
-                }
                 int now = br.read();
-                // EOF
                 if (now == -1) {
-                    printByteArray(bytes, bw);
+                    printByteArray(bytes, size, bw);
                     break;
                 }
-                if (bytes.size() <= 1) {
-                    bytes.add(now);
-                } else if (now == bytes.get(bytes.size() - 1)) {
-                    if (bytes.get(bytes.size() - 1) == bytes.get(bytes.size() - 2)) bytes.add(now);
-                    else {
-                        printByteArray(bytes, bw);
-                        bytes.clear();
-                        bytes.add(now);
+                size++;
+                bytes[size] = now;
+                if (size > 1) {
+                    if (bytes[0] == bytes[1] && bytes[1] != bytes[2]) {
+                        printByteArray(bytes, size, bw);
+                        size = -1;
                     }
-                } else {
-                    if (bytes.get(bytes.size() - 1) != bytes.get(bytes.size() - 2)) bytes.add(now);
-                    else {
-                        printByteArray(bytes, bw);
-                        bytes.clear();
-                        bytes.add(now);
+                    if (bytes[0] != bytes[1] && bytes[1] == bytes[2]) {
+                        printByteArray(bytes, size, bw);
+                        size = -1;
                     }
+                }
+                if (size == DEFINE_TYPE - 2) {
+                    printByteArray(bytes, size, bw);
+                    size = -1;
                 }
             }
         } catch (IOException e) {
@@ -73,12 +64,7 @@ public class Coder {
             while (true) {
                 int now = br.read();
                 if (now == -1) break;
-                if (now < DEFINE_TYPE) {
-                    char chatToOut = (char) br.read();
-                    for (int i = 0; i < now; i++) bw.write(chatToOut);
-                } else {
-                    for (int i = 0; i < now - DEFINE_TYPE; i++) bw.write((char) br.read());
-                }
+                for (int i = 0; i < now; i++) bw.write((char)br.read());
             }
         } catch (IOException e) {
             System.out.println("Can't find file");
