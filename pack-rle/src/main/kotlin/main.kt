d44d2c1f3a7580778299ee965bc42326d4454912
BeatusL <vagner.aa@edu.spbstu.ru>
2023-03-19 11:24:30 +0300

    encoder done (i think)

---
+++ pack-rle/src/main/kotlin/main.kt
@@ -0,0 +1,81 @@
+fun main() {
+}
+
+
+class Parser private constructor (private val text: String) {
+    private val originalLength = text.length
+    private fun encode(): String {
+        var index = 0
+        var result = ""
+
+        fun fromIntToString(count: Int, string: String): String {
+            var curCount = count
+            var res = ""
+            when {
+                count < 0 -> {
+                    var i = 1
+                    curCount *= -1
+                    if (curCount > 47) {
+                        while (curCount > 47) {
+                            res += "O" + string.substring(47 * (i - 1), 47 * i)
+                            i++
+                            curCount -= 47
+                        }
+                        res += ('!' + curCount - 1) + string.substring(47 * i)
+                    }
+                    res += ('!' + curCount - 1) + string
+                    return res
+                }
+                count > 1 -> {
+                    while (curCount > 47) {
+                        res += "~$string"
+                        curCount -= 47
+                    }
+                    res += if (curCount > 1) ('N' + curCount) + string
+                    else "!$string"
+                }
+                else -> throw IllegalArgumentException()
+            }
+            return res
+        }
+        // chars from !(33 ASCII) to O(79) represent count values from -1 to -47
+        // chars from P(80 ASCII) to ~(126) represent count values from 2 to 47
+        // full table can be seen in .resources
+
+        fun countDif() {
+            var count = 1
+            var cdResult = "${text[index]}"
+            while (index < text.length - 1 && text[index] != text[index + 1]) {
+                if (index < originalLength - 2 && text[index + 1] == text[index + 2]) break
+                count++
+                index++
+                cdResult += text[index]
+            }
+            result += fromIntToString(-count, cdResult)
+        }
+
+        fun countSim() {
+            var count = 1
+            while (index < text.length - 1 && text[index] == text[index + 1]) {
+                count++
+                index++
+            }
+            result += fromIntToString(count, text[index].toString())
+        }
+
+        while (index < originalLength) {
+            if (text[index] == text[index + 1]) {
+                countSim()
+            }
+            else countDif()
+            index++
+        }
+        return result
+    }
+    val encoded = encode()
+    val sizeRed = originalLength.toDouble() / encoded.length.toDouble()
+    companion object {
+        fun create(text: String) = Parser(text)
+    }
+}
+
---
+++ pack-rle/src/test/kotlin/test.kt
@@ -0,0 +1,21 @@
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Tests {
+    /*@Test
+    fun countDif() {
+        assertEquals(Pair("-7ABACABA", 6), Parser.create("ABACABA").countDif(0))
+    }
+    @Test
+    fun countSim() {
+        assertEquals(Pair("5A", 6), Parser.create("ABAAAAAb").countSim(2))
+    }*/
+
+    @Test
+    fun encode() {
+        val text = Parser.create("AAAABABABBBBBaba")
+        println(text.encoded)
+        println(text.sizeRed)
+    }
+}
