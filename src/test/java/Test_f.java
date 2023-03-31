c21b942f83b97890ee1ede23d57aa84e5a13cca8
Василиса <zhookvasilisad@gmail.com>
2023-03-31 12:47:15 +0300

    флаги

--- src/test/java/Test_f.java
+++ src/test/java/Test_f.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 public class Test_f {
+
+
     @Test
     public void flag_i(){
         final File expected = new File("C:\\Users\\Василиса\\IdeaProjects\\softdev-2023-spring-task-2\\output\\yeseninI.txt");
-        final File
+       // final File actual = new File()
     }
 }
