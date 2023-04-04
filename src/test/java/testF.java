94db5af8916b7fdac30548ad405c4a735f9ed1e8
Василиса <zhookvasilisad@gmail.com>
2023-04-05 00:18:51 +0300

    тестирование флагов

--- src/test/java/testF.java
+++ src/test/java/testF.java
@@ -17,7 +17,7 @@ public class testF {
 
     @Test
         public void flagC() throws IOException {
-        String args[] = {"-c", "input\\times.txt"};
+        String args[] = {"-c", "input\\повторы.txt"};
         consoleApp.main(args);
     }
 
@@ -30,6 +30,27 @@ public class testF {
     @Test
         public void flagO() throws IOException {
         String args[] = {"-o", "timesOut.txt", "-i", "input\\times.txt"};
+        consoleApp.main(args);
+    }
+
+    @Test
+       public void osiu() throws IOException {
+        String args[] = {"-o", "фейрверкSIU.txt", "-s", "22","-i", "u", "input\\фейрверк.txt"};
+        consoleApp.main(args);
+    }
+    @Test
+    public void oisu() throws IOException {
+        String args[] = {"-o", "фейрверкISU.txt","-i", "-s", "22", "u", "input\\фейрверк.txt"};
+        consoleApp.main(args);
+    }
+    @Test
+    public void uс() throws IOException {
+        String args[] = {"-u", "-c", "input\\монолог.txt"};
+        consoleApp.main(args);
+    }
+    @Test
+    public void cu() throws IOException {
+        String args[] = {"-c", "-u", "input\\монолог.txt"};
         consoleApp.main(args);
     }
 }
