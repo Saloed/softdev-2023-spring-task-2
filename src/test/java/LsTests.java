3c714ad51f5bf9e76f27bdd9ac420cfc8336337a
FeelingIcy <dobryachkasveta96@gmail.com>
2023-04-10 01:29:56 +0300

    New version of tests

--- src/test/java/LsTests.java
+++ src/test/java/LsTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,6 +10,7 @@ public class LsTests {
     public void testOutput(boolean l, boolean h, boolean r, Set<String> expected) throws IOException {
         String dir = "files";
         String o = "output.txt";
+        Set<String> result = new HashSet<>();
         Args args = new Args(l, h, r, o, dir);
         Main.start(args);
         File output = new File(args.out);
@@ -19,51 +21,52 @@ public class LsTests {
 
     @Test
     public void testL() throws IOException {
+        FileInfoList.setOfFiles.clear();
         testOutput(true, false, false,
                 Set.of("Vopros_10.docx 110 88041",
                 "Vopros_21.docx 110 55720",
                 "Vopros_3.docx 110 160181",
                 "Vopros_9.docx 110 74587"));
-        FileInfoList.setOfFiles.clear();
+
     }
 
   @Test
     public void testH() throws IOException {
+        FileInfoList.setOfFiles.clear();
         testOutput(true, true, false,
                 Set.of("Vopros_10.docx rw- 86,0KB",
                         "Vopros_21.docx rw- 54,4KB",
                         "Vopros_3.docx rw- 156,4KB",
                         "Vopros_9.docx rw- 72,8KB"));
-      FileInfoList.setOfFiles.clear();
     }
 
     @Test
     public void testR() throws IOException {
+        FileInfoList.setOfFiles.clear();
         testOutput(false, false, true,
                 Set.of("Vopros_9.docx",
                         "Vopros_3.docx",
                         "Vopros_21.docx",
                         "Vopros_10.docx"));
-        FileInfoList.setOfFiles.clear();
     }
 
     @Test
     public void testWithoutArguments() throws IOException {
+        FileInfoList.setOfFiles.clear();
         testOutput(false, false, false,
                 Set.of("Vopros_10.docx",
                         "Vopros_21.docx",
                         "Vopros_3.docx",
                         "Vopros_9.docx"));
-        FileInfoList.setOfFiles.clear();
     }
 
     @Test
     public void testLR() throws IOException {
+        FileInfoList.setOfFiles.clear();
         testOutput(true, false, true,
                 Set.of("Vopros_9.docx 110 74587",
                         "Vopros_3.docx 110 160181",
                         "Vopros_21.docx 110 55720",
                         "Vopros_10.docx 110 88041"));
-        FileInfoList.setOfFiles.clear();
     }
 }
