e976117164ff72a05a3e8353b179da83362aca2d
Chrisnisch <mihail.gusev.95@yandex.ru>
2023-04-12 18:55:03 +0300

    fixing tests

--- task/src/test/java/task2/FindTests.java
+++ task/src/test/java/task2/FindTests.java
@@ -20,7 +20,7 @@ public class FindTests {
         res.add(curDir + sep + "src" + sep + "test" + sep + "resources" + sep + "test1.txt");
         res.add(curDir + sep + "src" + sep + "test" + sep + "resources" + sep + "test2.txt");
         res.add(curDir + sep + "src" + sep + "test" + sep + "resources" + sep + "test20.txt");
-        System.out.println(new Parser().parse(input));
+        System.err.println(new Parser().parse(input));
         Assertions.assertEquals(new Parser().parse(input), res);
     }
 
@@ -30,7 +30,7 @@ public class FindTests {
         ArrayList<String> res = new ArrayList<>();
         res.add(curDir + sep + "src" + sep + "test" + sep + "resources" + sep + "test2.txt");
         res.add(curDir + sep + "src" + sep + "test" + sep + "resources" + sep + "test20.txt");
-        System.out.println(new Parser().parse(input));
+        System.err.println(new Parser().parse(input));
         Assertions.assertEquals(new Parser().parse(input), res);
     }
 
@@ -42,7 +42,7 @@ public class FindTests {
         res.add(curDir + sep + "build" + sep + "resources" + sep + "test" + sep + "test20.txt");
         res.add(curDir + sep + "src" + sep + "test" + sep + "resources" + sep + "test2.txt");
         res.add(curDir + sep + "src" + sep + "test" + sep + "resources" + sep + "test20.txt");
-        System.out.println(new Parser().parse(input));
+        System.err.println(new Parser().parse(input));
         Assertions.assertEquals(new Parser().parse(input), res);
     }
 
@@ -50,7 +50,7 @@ public class FindTests {
     public void test4() throws IOException {
         String[] input = {"someFileName"};
         ArrayList<String> res = new ArrayList<>();
-        System.out.println(new Parser().parse(input));
+        System.err.println(new Parser().parse(input));
         Assertions.assertEquals(new Parser().parse(input), res);
     }
 
@@ -59,7 +59,7 @@ public class FindTests {
         String[] input = {"build.gradle"};
         ArrayList<String> res = new ArrayList<>();
         res.add(curDir + "" + sep + "build.gradle.kts");
-        System.out.println(new Parser().parse(input));
+        System.err.println(new Parser().parse(input));
         Assertions.assertEquals(new Parser().parse(input), res);
     }
 
