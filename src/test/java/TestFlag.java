f11c4eb41dc5c65627411d8ad0e548453be2846f
Василиса <zhookvasilisad@gmail.com>
2023-04-14 11:01:44 +0300

    ;\

--- src/test/java/TestFlag.java
+++ src/test/java/TestFlag.java
@@ -5,25 +5,25 @@ public class TestFlag {
     @Test
     public void flagO() throws FileNotFoundException {
         UniqParser args = new UniqParser(false, false, false, 0,
-                false, true, "input\\фейрверкOut.txt", "input\\фейрверк.txt");
+                false, true, "input\\фейрверкOut.txt", "softdev-2023-spring-task-2\\input\\фейрверк.txt");
         ConsoleApp.start(args);
     }
     @Test
     public void flagIS() throws FileNotFoundException{
         UniqParser args = new UniqParser(true, false, true, 22,
-                false, false, "", "input\\фейрверк.txt");
+                false, false, "", "softdev-2023-spring-task-2\\input\\фейрверк.txt");
         ConsoleApp.start(args);
     }
     @Test
     public void flagCU() throws FileNotFoundException{
         UniqParser args = new UniqParser(false, true, false, 0,
-                true, false, "", "input\\монолог.txt");
+                true, false, "", "softdev-2023-spring-task-2\\input\\монолог.txt");
         ConsoleApp.start(args);
     }
     @Test
     public void flagUS() throws FileNotFoundException{
         UniqParser args = new UniqParser(true, true, true, 22,
-                false, false, "", "input\\фейрверк.txt");
+                false, false, "", "softdev-2023-spring-task-2\\input\\фейрверк.txt");
         ConsoleApp.start(args);
     }
 }
