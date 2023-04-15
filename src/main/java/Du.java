6cd290680e97ea069d8066a8ed1edc4812baabc4
likholetovks <liholetov.ks@edu.spbstu.ru>
2023-04-15 22:14:53 +0300

    Лихолетов Кирилл 3530901/20005 "Du" V8

--- src/main/java/Du.java
+++ src/main/java/Du.java
@@ -3,12 +3,9 @@ import org.apache.commons.cli.*;
 public class Du {
     public static void main(String[] args) {
         Options options = new Options();
-//        options.addOption("c","summary size");
-//        options.addOption("h","comfort for eye result");
-//        options.addOption("si","osnovanie 1000");
-        options.addOption(Option.builder("c").longOpt("summary-size").build());
-        options.addOption(Option.builder("h").longOpt("comfort-for-eye-result").build());
-        options.addOption(Option.builder("si").longOpt("osnovanie-1000").build());
+        options.addOption("c","summary size");
+        options.addOption("h","comfort for eye result");
+        options.addOption("si","osnovanie 1000");
         CommandLineParser parser = new DefaultParser();
         try {
             CommandLine cmd = parser.parse( options, args);
--- src/test/java/DuTest.java
+++ src/test/java/DuTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,52 +10,52 @@ public class DuTest {
 
     @Test
     public void test1(){
-        List<String> fileList = Arrays.asList("Inputs" + File.separator + "File1",
-                "Inputs" + File.separator + "File2",
-                "Inputs" + File.separator + "File3");
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File1").toString(),
+                Paths.get("Inputs", "File2").toString(),
+                Paths.get("Inputs", "File3").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, true, true);
         Assertions.assertEquals("208,74KB",functions.outForTest().trim());
     }
 
     @Test
     public void test2(){
-        List<String> fileList = Arrays.asList("Inputs" + File.separator + "File1",
-                "Inputs" + File.separator + "File2",
-                "Inputs" + File.separator + "File3");
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File1").toString(),
+                Paths.get("Inputs", "File2").toString(),
+                Paths.get("Inputs", "File3").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, false, true);
         Assertions.assertEquals("12,00B" + System.lineSeparator() + "124,36KB" + System.lineSeparator() + "84,37KB",functions.outForTest().trim());
     }
 
     @Test
     public void test3(){
-        List<String> fileList = Arrays.asList("Inputs" + File.separator + "File2",
-                "Inputs" + File.separator + "File1",
-                "Inputs" + File.separator + "File3");
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File2").toString(),
+                Paths.get("Inputs", "File1").toString(),
+                Paths.get("Inputs", "File3").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, false, true);
         Assertions.assertEquals("124,36KB" + System.lineSeparator() + "12,00B" + System.lineSeparator() + "84,37KB",functions.outForTest().trim());
     }
 
     @Test
     public void test4(){
-        List<String> fileList = Arrays.asList("Inputs" + File.separator + "File2",
-                "Inputs" + File.separator + "File1");
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File2").toString(),
+                Paths.get("Inputs", "File1").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, false, true);
         Assertions.assertEquals("124,36KB"+ System.lineSeparator() + "12,00B",functions.outForTest().trim());
     }
 
     @Test
     public void test5(){
-        List<String> fileList = Arrays.asList("Inputs" + File.separator + "File2",
-                "Inputs" + File.separator + "File1");
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File2").toString(),
+                Paths.get("Inputs", "File1").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, true, true);
         Assertions.assertEquals("124,37KB",functions.outForTest().trim());
     }
 
     @Test
     public void test6(){
-        List<String> fileList = Arrays.asList("Inputs" + File.separator + "File1",
-                "Inputs" + File.separator + "File2",
-                "Inputs" + File.separator + "File3");
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File1").toString(),
+                Paths.get("Inputs", "File2").toString(),
+                Paths.get("Inputs", "File3").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, true, false);
         Assertions.assertEquals("203,85KB",functions.outForTest().trim());
     }
