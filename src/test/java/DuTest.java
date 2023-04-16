bdc5865afea4de3d516a4553c45be307e42a7aff
likholetovks <liholetov.ks@edu.spbstu.ru>
2023-04-16 20:39:28 +0300

    Лихолетов Кирилл 3530901/20005 "Du" V10

--- src/test/java/DuTest.java
+++ src/test/java/DuTest.java
@@ -10,65 +10,59 @@ public class DuTest {
 
     @Test
     public void test1(){
-        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File1").toString().replace("\\", "/"),
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File1").toString(),
                 Paths.get("Inputs", "File2").toString(),
                 Paths.get("Inputs", "File3").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, true, true);
-        Assertions.assertEquals("208,74KB",functions.outForTest().trim());
+        Assertions.assertEquals("208.74KB",functions.outForTest().replace(',','.').trim());
     }
 
     @Test
     public void test2(){
-        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File1").toString().replace("\\", "/"),
-                Paths.get("Inputs", "File2").toString().replace("\\", "/"),
-                Paths.get("Inputs", "File3").toString().replace("\\", "/"));
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File1").toString(),
+                Paths.get("Inputs", "File2").toString(),
+                Paths.get("Inputs", "File3").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, false, true);
         Assertions.assertEquals("""
-                12,00B
-                124,36KB
-                84,37KB""",functions.outForTest().trim());
+                12.00B
+                124.36KB
+                84.37KB""",functions.outForTest().replace(',','.').trim());
     }
 
     @Test
     public void test3(){
-        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File2").toString().replace("\\", "/"),
-                Paths.get("Inputs", "File1").toString().replace("\\", "/"),
-                Paths.get("Inputs", "File3").toString().replace("\\", "/"));
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File2").toString(),
+                Paths.get("Inputs", "File1").toString(),
+                Paths.get("Inputs", "File3").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, false, true);
         Assertions.assertEquals("""
-                124,36KB
-                12,00B
-                84,37KB""",functions.outForTest().trim());
+                124.36KB
+                12.00B
+                84.37KB""",functions.outForTest().replace(',','.').trim());
     }
 
     @Test
     public void test4(){
-        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File2").toString().replace("\\", "/"),
-                Paths.get("Inputs", "File1").toString().replace("\\", "/"));
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File2").toString(),
+                Paths.get("Inputs", "File1").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, false, true);
-        Assertions.assertEquals("124,36KB\n12,00B",functions.outForTest().trim());
+        Assertions.assertEquals("124.36KB\n12.00B",functions.outForTest().replace(',','.').trim());
     }
 
     @Test
     public void test5(){
-        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File2").toString().replace("\\", "/"),
-                Paths.get("Inputs", "File1").toString().replace("\\", "/"));
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File2").toString(),
+                Paths.get("Inputs", "File1").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, true, true);
-        Assertions.assertEquals("124,37KB",functions.outForTest().trim());
+        Assertions.assertEquals("124.37KB",functions.outForTest().replace(',','.').trim());
     }
 
     @Test
     public void test6(){
-        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File1").toString().replace("\\", "/"),
-                Paths.get("Inputs", "File2").toString().replace("\\", "/"),
-                Paths.get("Inputs", "File3").toString().replace("\\", "/"));
+        List<String> fileList = Arrays.asList(Paths.get("Inputs", "File1").toString(),
+                Paths.get("Inputs", "File2").toString(),
+                Paths.get("Inputs", "File3").toString());
         Functions functions = new Functions(new Explorer(fileList).makingLongArrayList(), true, true, false);
-        Assertions.assertEquals("203,85KB",functions.outForTest().trim());
+        Assertions.assertEquals("203.85KB",functions.outForTest().replace(',','.').trim());
     }
-//    @Test
-//    public void test6() {
-//        String[] args = {"-c","-h","Inputs\\File1", "Inputs\\File2", "Inputs\\File3"};
-//        Du.main(args);
-//        Assertions.assertEquals("203,85KB",Du.answer.substring(0, Du.answer.length()-1));
-//    }
 }
